c40367328e3f2729249dfbe3ad44ba43b36c22cb
Michael Hoffmann <mhoffmann5@wisc.edu>
2022-07-28 20:25:28 -0500

    Fixed login issue

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,12 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
